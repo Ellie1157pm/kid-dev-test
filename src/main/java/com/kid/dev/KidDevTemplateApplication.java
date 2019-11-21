d3037a92f554e4eca410e27aff5d7045496ec82a
theshoon-git <theshoon1014@gmail.com>
2019-11-21 11:40:17 +0900

    first commit

---
+++ src/main/java/com/kid/dev/KidDevTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.kid.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidDevTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidDevTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kid/dev/KidDevTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kid.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidDevTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
